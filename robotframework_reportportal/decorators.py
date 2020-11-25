7d2f13ad4aa97ef04f4360ee1a3f9e95c2be6f68
German <petrff84@gmail.com>
2020-11-25 16:51:45 +0300

    Add decorators module

---
+++ robotframework_reportportal/decorators.py
@@ -0,0 +1,26 @@
+import time
+
+from robot.libraries.BuiltIn import BuiltIn
+
+
+def retry(exceptions_to_check=(Exception,), wait=3, retries=3):
+    """Decorator that provides retrying wrapped function in case of exception_to_check exception."""
+
+    def wrapped_retry(f):
+
+        @six.wraps(f)
+        def wrapped_f(*args, **kwargs):
+            attempts = retries + 1
+            for attempt in range(attempts):
+                try:
+                    return f(*args, **kwargs)
+                except exceptions_to_check as e:
+                    if attempt < retries:
+                        BuiltIn().log_to_console("%s, Retrying in %d seconds" % (e, wait))
+                        time.sleep(wait)
+                    else:
+                        BuiltIn().log_to_console('No more retries')
+                        raise
+        return wrapped_f
+
+    return wrapped_retry
